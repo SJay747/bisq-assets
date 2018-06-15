37b798081d9e26851a592ca03255929499061311
SJay747 <30780309+SJay747@users.noreply.github.com>
2018-06-15 17:23:41 +0100

    List BettingWin (BTW)

--- src/main/java/bisq/asset/tokens/BettingWin.java
+++ src/main/java/bisq/asset/tokens/BettingWin.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Bisq.
- *
+ * 
  * Bisq is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at
